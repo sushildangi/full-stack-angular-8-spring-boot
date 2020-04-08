0b89189b3fdb63ec2727a49691247b37d5683c9f
Sushil Dangi <dangi.sushil5@gmail.com>
2020-04-08 11:49:22 +0530

    first commit

---
+++ onlinebookstore/src/test/java/com/luv2tech/MainTests.java
@@ -0,0 +1,13 @@
+package com.luv2tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
